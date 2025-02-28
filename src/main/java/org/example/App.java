31a7b7ef4eaa840685e9e2ddbbd61ae3c274303f
4Siris <leonid_voynov@mail.ru>
2025-02-28 20:42:01 +0300

    init : init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MainFrame.start();
+
+    }
+}
